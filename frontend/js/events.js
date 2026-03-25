776cf4bb8efe9df54bd6f119be2460be42ba0cdc
Tanase George(Jorj) <tanase.george.1905@gmail.com>
2026-03-25 18:15:27 +0200

    Login and sign up page

--- frontend/js/events.js
+++ frontend/js/events.js
@@ -1,1 +1,14 @@
 // ===== BUTOANE & EVENTS =====
+
+var button = document.getElementById("reportForm");
+ button.onclick = onbutton;
+ function onbutton() {
+     if(onbutton()){
+         button.style.backgroundColor = "green";
+         button.disabled=true;
+     }else{
+         button.style.backgroundColor = "red";
+         button.disabled=false;
+
+     }
+ }
